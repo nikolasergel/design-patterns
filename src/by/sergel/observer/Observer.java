c6265fda566de5ba3b8faafc95966f9bcde7ea41
Nikolay Sergel <nikolasergel19@gmail.com>
2021-03-18 13:12:46 +0300

    Observer: initial commit

---
+++ src/by/sergel/observer/Observer.java
@@ -0,0 +1,5 @@
+package by.sergel.observer;
+
+public interface Observer {
+    void update(String message);
+}
---
+++ src/by/sergel/observer/User.java
@@ -0,0 +1,10 @@
+package by.sergel.observer;
+
+public class User implements Observer{
+    private Subject subject;
+
+    @Override
+    public void update(String message) {
+
+    }
+}
